a1c0b9885cb3b59d91a765239a65dabcc88113bb
Twesigye Anthony <twesigye10@gmail.com>
2025-11-10 20:53:43 +0300

    database setup and started on api implementation

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,36 +1,35 @@
 /*jshint esversion: 8 */
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const pinoLogger = require('./logger');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const pinoLogger = require("./logger");
 
-const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
-
+const connectToDatabase = require("./models/db");
+const { loadData } = require("./util/import-mongo/index");
 
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
-
+connectToDatabase()
+  .then(() => {
+    pinoLogger.info("Connected to DB");
+  })
+  .catch((e) => console.error("Failed to connect to DB", e));
 
 app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+const giftRoutes = require("./routes/giftRoutes");
+app.use("/api/gifts", giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
 
-
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
+const pinoHttp = require("pino-http");
+const logger = require("./logger");
 
 app.use(pinoHttp({ logger }));
 
@@ -41,17 +40,16 @@ app.use(pinoHttp({ logger }));
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
 
-
 // Global Error Handler
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
+  console.error(err);
+  res.status(500).send("Internal Server Error");
 });
 
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+app.get("/", (req, res) => {
+  res.send("Inside the server");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
